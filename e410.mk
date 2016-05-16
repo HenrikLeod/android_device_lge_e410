613360a011038ce73cfa946df4d30fec952fccc2
chijure <chijure100x100to@gmail.com>
2016-05-15 20:04:39 -0500

    Bye msm7x27a-common bye NFc support
    Signed-off-by: Jhuan Reategui <chijure100x100to@gmail.com>

--- e410.mk
+++ e410.mk
@@ -1,12 +1,32 @@
-$(call inherit-product, device/lge/msm7x27a-common/msm7x27a-common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product-if-exists, vendor/lge/e410/e410-vendor.mk)
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/lge/e410/overlay
 
 $(call inherit-product, $(LOCAL_PATH)/mdpi-dalvik.mk)
 
 PRODUCT_AAPT_CONFIG := normal mdpi
+
+$(call inherit-product-if-exists, vendor/lge/e410/e410-vendor.mk)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init.lge-shared.rc:root/init.lge-shared.rc \
+    $(LOCAL_PATH)/init.lge.usb.rc:root/init.lge.usb.rc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
+    $(LOCAL_PATH)/configs/AudioFilter.csv:system/etc/AudioFilter.csv \
+    $(LOCAL_PATH)/configs/vold.fstab:system/etc/vold.fstab
+
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init.v1.rc:root/init.v1.rc \
@@ -27,7 +47,72 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     gps.e410
 
+$(call inherit-product, build/target/product/full.mk)
+
+# Permission files
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
+
+PRODUCT_PACKAGES += \
+    lgapversion
+
+# Wifi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+
+# BT
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/init.qcom.bt.sh:system/etc/init.qcom.bt.sh
+
+# HW HALS
+PRODUCT_PACKAGES += \
+    libgenlock \
+    liboverlay \
+    gralloc.msm7x27a \
+    hwcomposer.msm7x27a \
+    copybit.msm7x27a \
+    audio.a2dp.default \
+    audio.primary.msm7x27a \
+    audio_policy.msm7x27a \
+    charger \
+    charger_res_images
+
+# OMX 
+PRODUCT_PACKAGES += \
+    libstagefrighthw \
+    libmm-omxcore \
+    libOmxCore
+
+# Common properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.fb.rgb565=0 \
+    debug.sf.hw=1 \
+    debug.composition.type=mdp \
+    ro.opengles.version=131072 \
+    com.qc.hardware=true \
+    persist.sys.use_dithering=1 \
+    wifi.interface=wlan0 \
+    rild.libpath=/system/lib/libril-qc-1.so \
+    ro.telephony.ril_class=LGEQualcommUiccRIL \
+    ro.telephony.default_network=0 \
+    telephony.lteOnGsmDevice=0 \
+    persist.sys.usb.config=mtp,adb \
+    hwui.render_dirty_regions=true \
+    ro.max.fling_velocity=4000 \
+    sys.mem.max_hidden_apps=3
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_BRAND := lge
 PRODUCT_NAME := full_e410
 PRODUCT_DEVICE := e410
 PRODUCT_MODEL := LG-E410
-
